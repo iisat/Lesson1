63800145e97cede655f7de4c15eed93b08e4ce50
iisat <4053846@gmail.com>
2018-11-25 11:23:17 +0300

    Lesson8

--- Lesson8/task1.py
+++ Lesson8/task1.py
@@ -2,7 +2,9 @@
 # Например, состоящая только из маленьких латинских букв. Требуется найти количество различных подстрок
 # в этой строке. Для решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или
 # встроенную функцию hash()
+import collections
 
+# Используем обычный массив
 def count_substrings(s):
     hashesarray = []
     for i in range(0, len(s)):
@@ -11,4 +13,13 @@ def count_substrings(s):
                 hashesarray.append(hash(s[i:j + 1]))
     return (len(hashesarray) - 2)
 
-print(count_substrings('mama'))
+# Используем словарь
+def mod_count_substrings(s):
+    d = collections.OrderedDict()
+    for i in range(0, len(s)):
+        for j in range(0, len(s)):
+            d.update({s[i:j + 1]:hash(s[i:j + 1])})
+    return (len(d) - 2)
+
+
+print(mod_count_substrings('mama'))
